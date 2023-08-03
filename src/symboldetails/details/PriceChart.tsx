3f27360761cdbd1843f00d75e026cc0916329933
Andreas Wassum <a.wassum@gmail.com>
2023-08-03 23:34:24 +0200

    Refetching historical prices on tab switching

--- src/symboldetails/details/PriceChart.tsx
+++ src/symboldetails/details/PriceChart.tsx
@@ -1,39 +1,31 @@
 import {Area, AreaChart, CartesianGrid, ResponsiveContainer, Tooltip, XAxis, YAxis} from "recharts";
-import {Box, CircularProgress, Tab} from "@mui/material";
-import {useQuery} from "@tanstack/react-query";
+import {Box, Tab} from "@mui/material";
 import {useApiWrapper} from "../../config/apiWrapper";
-import {GET_HISTORIC_PRICES, getHistoricPricesConfig} from "../api/symbolDetailsApi";
 import {HistoricalPrices} from "../../stock/stockTypes";
-import ErrorComponent from "../../error/ErrorComponent";
-import {SyntheticEvent, useState} from "react";
+import {SyntheticEvent, useEffect, useState} from "react";
 import {TabContext, TabList, TabPanel} from "@mui/lab";
 import {Period} from "../symbolTypes";
+import {getHistoricPricesConfig} from "../api/symbolDetailsApi";
 
 
 export const PriceChart = ({symbol}: { symbol: string }) => {
-    const [tabValue, setTabValue] = useState("1");
-
-    const handleChange = (event: SyntheticEvent, newValue: string) => {
-        setTabValue(newValue);
-    };
-
+    const [tabValue, setTabValue] = useState<Period>(Period.THIS_YEAR);
+    const [priceList, setPriceList] = useState<HistoricalPrices[]>([]);
     const {apiGet} = useApiWrapper();
 
-    const {isLoading, isFetching, error, data} = useQuery<HistoricalPrices>(
-        [GET_HISTORIC_PRICES, symbol],
-        () => apiGet(getHistoricPricesConfig(symbol, Period.THIS_YEAR))
-    );
+    useEffect(() => {
+        apiGet(getHistoricPricesConfig(symbol, tabValue))
+            .then(response => setPriceList(response.historicalPriceList))
+    }, [tabValue])
 
-    if (isLoading || isFetching) return <CircularProgress/>
-
-    if (error) return <ErrorComponent errorMessage={error as string}/>
-
-    const {historicalPriceList} = data!
+    const handleTabChange = (event: SyntheticEvent, newValue: Period) => {
+        setTabValue(newValue);
+    };
 
     const getResponsiveContainer = () => {
         return (
             <ResponsiveContainer width="100%" height={300}>
-                <AreaChart data={historicalPriceList} margin={{top: 5, right: 20, bottom: 5, left: 0}}>
+                <AreaChart data={priceList} margin={{top: 5, right: 20, bottom: 5, left: 0}}>
                     <defs>
                         <linearGradient id="colorPrice" x1="0" y1="0" x2="0" y2="1">
                             <stop offset="5%" stopColor="#82ca9d" stopOpacity={0.8}/>
@@ -54,28 +46,25 @@ export const PriceChart = ({symbol}: { symbol: string }) => {
         );
     }
 
-
     return (
         <Box id="priceChart" sx={{marginTop: "10%", width: "80%"}}>
             <TabContext value={tabValue}>
-                <Box sx={{ borderBottom: 1, borderColor: 'divider' }}>
-                    <TabList onChange={handleChange} aria-label="Historic Stock Price">
-                        <Tab label="1 Week" value="1" />
-                        <Tab label="1 Month" value="2" />
-                        <Tab label="6 Months" value="3" />
-                        <Tab label="This Year" value="4" />
-                        <Tab label="1 Year" value="5" />
-                        <Tab label="5 Years" value="6" />
-                        <Tab label="Max" value="7" />
+                <Box sx={{borderBottom: 1, borderColor: 'divider'}}>
+                    <TabList onChange={handleTabChange} aria-label="Historic Stock Price">
+                        <Tab label="1 Month" value={Period.MONTH1}/>
+                        <Tab label="6 Months" value={Period.MONTH6}/>
+                        <Tab label="This Year" value={Period.THIS_YEAR}/>
+                        <Tab label="1 Year" value={Period.YEAR1}/>
+                        <Tab label="5 Years" value={Period.YEAR5}/>
+                        <Tab label="Max" value={Period.MAX}/>
                     </TabList>
                 </Box>
-                <TabPanel value="1">{getResponsiveContainer()}</TabPanel>
-                <TabPanel value="2">{getResponsiveContainer()}</TabPanel>
-                <TabPanel value="3">{getResponsiveContainer()}</TabPanel>
-                <TabPanel value="4">{getResponsiveContainer()}</TabPanel>
-                <TabPanel value="5">{getResponsiveContainer()}</TabPanel>
-                <TabPanel value="6">{getResponsiveContainer()}</TabPanel>
-                <TabPanel value="7">{getResponsiveContainer()}</TabPanel>
+                <TabPanel value={Period.MONTH1}>{getResponsiveContainer()}</TabPanel>
+                <TabPanel value={Period.MONTH6}>{getResponsiveContainer()}</TabPanel>
+                <TabPanel value={Period.THIS_YEAR}>{getResponsiveContainer()}</TabPanel>
+                <TabPanel value={Period.YEAR1}>{getResponsiveContainer()}</TabPanel>
+                <TabPanel value={Period.YEAR5}>{getResponsiveContainer()}</TabPanel>
+                <TabPanel value={Period.MAX}>{getResponsiveContainer()}</TabPanel>
             </TabContext>
         </Box>
     );
