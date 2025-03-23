99ebcffc29d634326cfa33e67bf0f491763b9628
Andreas Wassum <a.wassum@gmail.com>
2025-03-23 21:45:41 +0100

    Simplified root structure

--- src/root.tsx
+++ src/root.tsx
@@ -12,7 +12,16 @@ import SymbolDetailsPage from "./pages/symboldetails/SymbolDetailsPage";
 import DefaultLayout from "./DefaultLayout";
 import ContestPage from "./pages/contest/ContestPage";
 import ContestsPage from "./pages/contests/ContestsPage";
-
+import {CssBaseline, ThemeProvider} from "@mui/material";
+import {myTheme} from "./styles/theme/myTheme";
+import {LocalizationProvider} from "@mui/x-date-pickers";
+import {AdapterDateFns} from "@mui/x-date-pickers/AdapterDateFnsV3";
+import {QueryClientProvider} from "@tanstack/react-query";
+import {queryClient} from "./config/queryConfig";
+import {ReactQueryDevtools} from "@tanstack/react-query-devtools";
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./root.css";
 
 const ProtectedComponent = () => {
     return (
@@ -56,19 +65,22 @@ const router = createBrowserRouter([
                 {path: "admin/users", element: <AdminUsers/>}
             ]
         },
-    ],
-    {
-        future: {
-            v7_skipActionErrorRevalidation: true,
-            v7_normalizeFormMethod: true,
-            v7_relativeSplatPath: true,
-            v7_fetcherPersist: true,
-            v7_partialHydration: true,
-        },
-    });
+    ]
+);
 
-export const Body = () => {
-    return (
-        <RouterProvider router={router} future={{v7_startTransition: true}}/>
-    );
-}
+const root = ReactDOM.createRoot(document.getElementById("root") as HTMLElement);
+
+root.render(
+    <React.StrictMode>
+        <ThemeProvider theme={myTheme}>
+            <LocalizationProvider dateAdapter={AdapterDateFns}>
+                <QueryClientProvider client={queryClient}>
+                    <CssBaseline>
+                        <RouterProvider router={router}/>
+                    </CssBaseline>
+                    <ReactQueryDevtools initialIsOpen={false}/>
+                </QueryClientProvider>
+            </LocalizationProvider>
+        </ThemeProvider>
+    </React.StrictMode>
+);
