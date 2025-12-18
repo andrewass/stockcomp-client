44de5b813ad1bef7f47cb30cf9f1dd5e438f1096
Andreas Wassum <a.wassum@gmail.com>
2025-12-18 22:39:40 +0100

    Deleted some more files

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,35 +1,34 @@
 import React from "react";
-import { Metadata } from "next";
-import AppThemeProvider from "../theme/AppThemeProvider.tsx";
-import {LocalizationProvider} from "@mui/x-date-pickers";
-import {AdapterDateFns} from "@mui/x-date-pickers/AdapterDateFns";
-import {QueryClientProvider} from "@tanstack/react-query";
-import {queryClient} from "../config/queryConfig.ts";
-import {ReactQueryDevtools} from "@tanstack/react-query-devtools";
+import {Metadata} from "next";
 
 export const metadata: Metadata = {
-	title: "Stock Comp",
-	description: "Stock Comp is a web app for arranging stock trade contests.",
+    title: "Stock Comp",
+    description: "Stock Comp is a web app for arranging stock trade contests.",
 };
 
+/**
+ *             <AppThemeProvider>
+ *                 <QueryClientProvider client={queryClient}>
+ *                     {children}
+ *                     <ReactQueryDevtools initialIsOpen={false}/>
+ *                 </QueryClientProvider>
+ *             </AppThemeProvider>{children}</div>
+ * @param children
+ * @constructor
+ */
+
 export default function RootLayout({
-	children,
-}: {
-	children: React.ReactNode;
+                                       children,
+                                   }: {
+    children: React.ReactNode;
 }) {
-	return (
-		<html lang="en">
-			<body>
-				<div id="root">
-                    <AppThemeProvider>
-                        <LocalizationProvider dateAdapter={AdapterDateFns}>
-                            <QueryClientProvider client={queryClient}>
-                                {children}
-                                <ReactQueryDevtools initialIsOpen={false} />
-                            </QueryClientProvider>
-                        </LocalizationProvider>
-                    </AppThemeProvider>{children}</div>
-			</body>
-		</html>
-	);
+    return (
+        <html lang="en">
+        <body>
+        <div id="root">
+            <main>{children}</main>
+        </div>
+        </body>
+        </html>
+    );
 }
---
+++ src/app/page.tsx
@@ -0,0 +1,23 @@
+
+export default function MainPage() {
+    return (
+        <div>
+            <p>Hello</p>
+        </div>
+    );
+}
+
+/*
+export default function App() {
+	return (
+		<AppThemeProvider>
+			<LocalizationProvider dateAdapter={AdapterDateFns}>
+				<QueryClientProvider client={queryClient}>
+					<ReactQueryDevtools initialIsOpen={false} />
+				</QueryClientProvider>
+			</LocalizationProvider>
+		</AppThemeProvider>
+	);
+}
+
+ */
