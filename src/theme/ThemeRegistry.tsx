e51110215eac8c46a268352429255e9c0a3617cd
Andreas Wassum <a.wassum@gmail.com>
2025-12-20 21:56:24 +0100

    Setting queryClient and provider

--- src/theme/ThemeRegistry.tsx
+++ src/theme/ThemeRegistry.tsx
@@ -2,7 +2,9 @@
 
 import { CssBaseline, ThemeProvider } from "@mui/material";
 import { AppRouterCacheProvider } from "@mui/material-nextjs/v16-appRouter";
+import { QueryClientProvider } from "@tanstack/react-query";
 import { type ReactNode, useEffect, useState } from "react";
+import { queryClient } from "../config/queryConfig.ts";
 import { ThemeContext, type ThemeMode } from "./ThemeContext.ts";
 import {
 	darkTheme,
@@ -25,14 +27,12 @@ export default function ThemeRegistry({ children }: { children: ReactNode }) {
 		}
 	}, []);
 
-	const toggleTheme = () => {
-		const currentThemeIsLight = themeMode === lightThemeMode;
-		setThemeMode(currentThemeIsLight ? darkThemeMode : lightThemeMode);
-		localStorage.setItem(
-			THEME_STORAGE_KEY,
-			currentThemeIsLight ? darkThemeMode : lightThemeMode,
-		);
-	};
+	function toggleTheme() {
+		const newThemeMode =
+			themeMode === lightThemeMode ? darkThemeMode : lightThemeMode;
+		setThemeMode(newThemeMode);
+		localStorage.setItem(THEME_STORAGE_KEY, newThemeMode);
+	}
 
 	return (
 		<AppRouterCacheProvider>
@@ -40,8 +40,10 @@ export default function ThemeRegistry({ children }: { children: ReactNode }) {
 				<ThemeProvider
 					theme={themeMode === lightThemeMode ? lightTheme : darkTheme}
 				>
-					<CssBaseline />
-					{children}
+					<QueryClientProvider client={queryClient}>
+						<CssBaseline />
+						{children}
+					</QueryClientProvider>
 				</ThemeProvider>
 			</ThemeContext.Provider>
 		</AppRouterCacheProvider>
